25ec511ac0a2117cac275d902fc83462aaea1fd7
sename81 <44600474+sename81@users.noreply.github.com>
2026-04-30 10:38:43 +0800

    Create app.py

---
+++ app.py
@@ -0,0 +1,15 @@
+import streamlit as st
+import pandas as pd
+
+st.title("RE-HI Tool")
+
+file = st.file_uploader("Upload Excel or CSV", type=["xlsx","xls","csv"])
+
+if file:
+    if file.name.endswith(".csv"):
+        df = pd.read_csv(file)
+    else:
+        df = pd.read_excel(file)
+
+    st.success("File loaded")
+    st.dataframe(df)
